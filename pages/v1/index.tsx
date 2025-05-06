b26408f251e6c9a327d94adb5790076cc0f8bf8c
hod25 <hod7112000@gmail.com>
2025-05-06 18:13:46 +0300

    v1

---
+++ pages/v1/index.tsx
@@ -0,0 +1,103 @@
+"use client";;
+
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import ReadmeViewer from '../../components/ReadmeViewer'; 
+
+import RepoCard from "../../components/RepoCard";
+import { fetchRepos } from "../../lib/github";
+
+type ProjectCardProps = {
+  title: string;
+  description: string;
+  link?: string;
+};
+
+
+export default function Home() {
+  const username = "hod25"; 
+  const [repos, setRepos] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchRepositories = async () => {
+      const fetchedRepos = await fetchRepos(username);
+      setRepos(fetchedRepos);
+    };
+
+    fetchRepositories();
+  }, []);
+
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  // זיהוי המצב של המערכת והגדרת המצב הראשוני
+  useEffect(() => {
+    // אם יש כבר הגדרה מקומית שמורה
+    const savedMode = localStorage.getItem('darkMode');
+    if (savedMode) {
+      setDarkMode(savedMode === 'true');
+    } else {
+      // אם אין הגדרה מקומית, נגלה לפי הגדרת המערכת
+      const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      setDarkMode(prefersDarkMode);
+    }
+  }, []);
+
+  // עדכון ה-class של ה-body לפי מצב המצב כהה
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+    // שמירה על המצב של המצב כהה
+    localStorage.setItem('darkMode', darkMode.toString());
+  }, [darkMode]);
+
+  return (
+    <div>
+      <Head>
+      <link rel="icon" href="/favicon.ico" />
+        <title>Hod Mitrany - Portfolio</title>
+        <meta name="description" content="Portfolio of Hod Mitrany - Software Developer" />
+      </Head>
+      <main className="container">
+        {/* כפתור שינוי מצב כהה */}
+        <button
+          className="dark-mode-toggle"
+          onClick={() => setDarkMode(!darkMode)}
+        >
+        {darkMode ? 'Light Mode' : 'Dark Mode'}
+        </button>
+        <h1>Hello, I'm Hod Mitrany</h1>
+        <p>I'm a Computer Science student passionate about backend development, DevOps, full-stack web apps, and IoT projects. Here are some of my works:</p>
+        <div className="profile-links">
+          <a href="https://www.linkedin.com/in/hod-mitrany/" target="_blank">LinkedIn</a>
+          <a href="https://github.com/hod25" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="/Hod_Mitrany_CV.pdf" target="_blank">Resume (PDF)</a>
+          <a href="https://docs.google.com/document/d/1oCLnmsEQrpIXQk7-LObgFLzX1nPyVlUW/edit?usp=sharing&ouid=100594456677724045614&rtpof=true&sd=true" target="_blank">Resume (Docs)</a>
+        </div>
+        <div className="try-button-container">
+          <a href="/search" className="try-button">Try it yourself 🔍</a>
+        </div>
+        <section className="projects-section">
+          <h2>Highlighted Projects</h2>
+          <div className="projects-grid">
+ 
+            {repos.map((repo: any) => (
+            <RepoCard
+            key={repo.id}
+            name={repo.name}
+            description={repo.description}
+            html_url={repo.html_url}
+            language={repo.language}
+            />
+          ))}
+          </div>
+          </section>
+          </main>
+      <footer className="site-footer">
+        © {new Date().getFullYear()} Built by Hod Mitrany
+      </footer>
+    </div>
+  );  
+}
